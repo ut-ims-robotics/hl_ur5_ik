ac197566a78942970304086ec65713feadf427ee
gryogor <gryogor918@gmail.com>
2021-11-16 13:03:25 +0200

    added service call to compute trajectory

--- src/ur_ik_request.cpp
+++ src/ur_ik_request.cpp
@@ -1,41 +1,104 @@
 #include "ros/ros.h"
 #include "moveit_msgs/GetPositionIK.h"
+#include "moveit_msgs/GetMotionPlan.h"
+#include "moveit_msgs/Constraints.h"
+#include "moveit_msgs/JointConstraint.h"
 #include "sensor_msgs/JointState.h"
 #include "geometry_msgs/PoseStamped.h"
 #include <iostream>
+#include "pluginlib/class_loader.h"
+
+
+#include "moveit/robot_model_loader/robot_model_loader.h"
+#include "moveit/move_group_interface/move_group_interface.h"
+#include "moveit/planning_scene_interface/planning_scene_interface.h"
+#include <moveit/planning_scene/planning_scene.h>
+#include "moveit/trajectory_execution_manager/trajectory_execution_manager.h"
+
 
 geometry_msgs::PoseStamped end_effector_pos = geometry_msgs::PoseStamped();
+
+static const std::string PLANNING_GROUP = "manipulator";
 
 class ik_request
 {
 private:
-  sensor_msgs::JointState current_Joint;
-  moveit_msgs::GetPositionIK _request;
+  sensor_msgs::JointState _current_Joint;
+  moveit_msgs::GetPositionIK _request_ik;
+  moveit_msgs::GetMotionPlan _request_trajectory;
+  moveit_msgs::JointConstraint _goalPosition;
+
   ros::NodeHandle _nh;
-  ros::ServiceClient client;
+  ros::ServiceClient _client_ik;
+  ros::ServiceClient _client_trajectory;
 public:
   ik_request(ros::NodeHandle nh)
   {
-    current_Joint = sensor_msgs::JointState();
-    _request.request.ik_request.robot_state.joint_state = current_Joint;
-    _request.request.ik_request.group_name = "manipulator";
-    
-    _request.request.ik_request.pose_stamped = geometry_msgs::PoseStamped();
-    _request.request.ik_request.pose_stamped.header.frame_id = "world";
+    _current_Joint = sensor_msgs::JointState();
+    _request_ik.request.ik_request.robot_state.joint_state = _current_Joint;
+    _request_ik.request.ik_request.group_name = "manipulator";    
+    _request_ik.request.ik_request.pose_stamped = geometry_msgs::PoseStamped();
+    _request_ik.request.ik_request.pose_stamped.header.frame_id = "world";
+
+    _request_trajectory.request.motion_plan_request.group_name = "manipulator";
+    //_request_trajectory.request.motion_plan_request.start_state.joint_state = _current_Joint;
+
     _nh = nh;
-    client = _nh.serviceClient<moveit_msgs::GetPositionIK>("compute_ik");
+    _client_ik = _nh.serviceClient<moveit_msgs::GetPositionIK>("compute_ik");
+    _client_trajectory = _nh.serviceClient<moveit_msgs::GetMotionPlan>("plan_kinematic_path");
+    
   }
 
   sensor_msgs::JointState IK_compute(geometry_msgs::PoseStamped end_effector_position)
   {
-    _request.request.ik_request.pose_stamped = end_effector_position;
-    _request.request.ik_request.pose_stamped.header.frame_id = "world";
-    if (client.call(_request))
+    _request_ik.request.ik_request.pose_stamped = end_effector_position;
+    _request_ik.request.ik_request.pose_stamped.header.frame_id = "world";
+    if (_client_ik.call(_request_ik))
     {
-      _request.request.ik_request.robot_state.joint_state = _request.response.solution.joint_state;
-      std::cout << _request.response.error_code << std::endl;
+      _request_ik.request.ik_request.robot_state.joint_state = _request_ik.response.solution.joint_state;
+      std::cout << _request_ik.response.error_code << std::endl;
     }
-    return _request.request.ik_request.robot_state.joint_state;
+    return _request_ik.request.ik_request.robot_state.joint_state;
+  }
+
+  void Trajectory_execute(sensor_msgs::JointState goalState)
+  {
+      //TODO
+      /*
+       * use /plan_kinematic_path service to create a plan(kinda done?) an then execute it.
+       */
+
+
+    int size_of_vector = goalState.name.size();
+    
+    /*
+    name: 
+  - shoulder_pan_joint
+  - shoulder_lift_joint
+  - elbow_joint
+  - wrist_1_joint
+  - wrist_2_joint
+  - wrist_3_joint
+     */
+    moveit_msgs::Constraints goal;
+
+
+    for (size_t i = 0; i < size_of_vector; i++)
+    {
+      _goalPosition.joint_name = goalState.name[i];
+      _goalPosition.position = goalState.position[i];
+      _goalPosition.weight = 1;
+      goal.joint_constraints.push_back(_goalPosition);
+    }
+    
+
+
+    _request_trajectory.request.motion_plan_request.goal_constraints.push_back(goal);
+
+    if (_client_trajectory.call(_request_trajectory))
+    {
+      //make the push and execute of trajectory
+    }
   }
 
 };
@@ -59,10 +122,22 @@ int main(int argc, char **argv)
 
   sensor_msgs::JointState joint_to_send = sensor_msgs::JointState();
   ros::Rate loop_rate(10);
+
+
   while (ros::ok())
   {
-    joint_to_send = ik_computation.IK_compute(end_effector_pos);
-    joint_pub.publish(joint_to_send);
+    if (end_effector_pos.header.stamp.sec == 1)
+    {
+      //TODO
+      /*
+      * function to use generated joint states to make a trajectory and execute
+      */
+    }
+    else
+    {
+      joint_to_send = ik_computation.IK_compute(end_effector_pos);
+      joint_pub.publish(joint_to_send);
+    }
     loop_rate.sleep();
     ros::spinOnce();
   }
